126c92dcafe63221233f4c1f82ab19be9b964797
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-22 22:51:19 +0100

    Initial commit and complete with h1 and v-bind for image

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello world',
+      image: 'src/img/b31d57987dbe0b837e8336cec98e18b9.jpg',
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <img :src="image">
+</template>
+
+<style></style>

d427ec30ddd6e3688d6ccec1811a5c1950fcda0a
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-22 23:07:43 +0100

    add v-on and v-model

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@ export default {
     return {
       message: 'Hello world',
       image: 'src/img/b31d57987dbe0b837e8336cec98e18b9.jpg',
+      date: '',
+    }
+  },
+  methods: {
+    revenge() {
+      console.log('V per Vendetta', this.message);
     }
   }
 }
@@ -14,6 +20,11 @@ export default {
 <template>
   <h1>{{ message }}</h1>
   <img :src="image">
+
+  <button @click="revenge">click me</button>
+
+  <input type="text" placeholder="Type a date" v-model="date">
+  <div>{{ date }}</div>
 </template>
 
 <style></style>
